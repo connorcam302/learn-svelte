85ac9b07f425c21b2e74e1d2709352afd1a071b4
Connor Campbell <connorcam123@gmail.com>
2023-08-07 00:34:47 +0100

    Initial github commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	type bossType = {
+		name: string;
+		totalDeaths: number;
+		userDeaths: number;
+		avgDeaths: number;
+	};
+
+	function addDeath(boss: bossType) {
+		console.log(boss);
+		boss.userDeaths += 1;
+		boss.totalDeaths += 1;
+		return boss;
+	}
+
+	let abysswatchers: bossType = {
+		name: 'Abyss Watchers',
+		totalDeaths: 85,
+		userDeaths: 0,
+		avgDeaths: 17
+	};
+	let bosses: bossType[] = [
+		{
+			name: 'Abyss Watchers',
+			totalDeaths: 85,
+			userDeaths: 1,
+			avgDeaths: 17
+		},
+		{
+			name: 'Pontiff Sulyvahn',
+			totalDeaths: 156,
+			userDeaths: 5,
+			avgDeaths: 31.2
+		},
+		{
+			name: 'Champion Gundyr',
+			totalDeaths: 50,
+			userDeaths: 3,
+			avgDeaths: 10
+		}
+	];
+</script>
+
+<div class="w-full font-bold">
+	{#each bosses as boss}
+		<div class="">
+			<h3>{boss.name}</h3>
+			<h4>{boss.userDeaths}</h4>
+			<button on:click={() => (boss = addDeath(boss))}>Add Death</button>
+		</div>
+	{/each}
+</div>
